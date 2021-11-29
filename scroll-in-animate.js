3a320420896ff69bce417f2e20dcff48308a98d9
bartguz <bartguz@gizer.io>
2021-11-29 22:35:38 +0100

    animatrix

--- scroll-in-animate.js
+++ scroll-in-animate.js
@@ -3,7 +3,7 @@
     'use strict';
   
     // define variables
-    var items = document.querySelectorAll(".timeline li");
+    var items = document.querySelectorAll(".timeline li, .scroll-in-animate");
   
     // check if an element is in viewport
     // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
